dbbcb41b5d6f60b5bef0e68be8ff86ef1539453d
Islam Mesha <eng.islammesha@gmail.com>
2019-02-19 11:06:36 +0200

    Manage the app.

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/media/islam_mesha/IT-Blocks/Playground/Django/Friends/venv/bin/python2
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
