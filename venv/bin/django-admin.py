03ae4f7e307155cda29ce94c15a91ca38f4b3148
Islam Mesha <eng.islammesha@gmail.com>
2019-02-19 10:56:49 +0200

    Init app.

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/media/islam_mesha/IT-Blocks/Playground/Django/Friends/venv/bin/python2
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
